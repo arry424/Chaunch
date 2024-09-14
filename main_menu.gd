06a815beeb5179f169cefb5485d21f2db37c54b4
arry424 <165412663+arry424@users.noreply.github.com>
2024-09-14 18:13:33 -0400

    how to play added

--- main_menu.gd
+++ main_menu.gd
@@ -8,3 +8,7 @@ func _on_play_button_button_down():
 
 func _on_quit_button_button_down():
 	get_tree().quit()
+
+
+func _on_how_to_play_button_down():
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
