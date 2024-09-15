e7a1b3502fd9584186b9077b22420c68cf773853
arry424 <165412663+arry424@users.noreply.github.com>
2024-09-15 00:13:29 -0400

    added a spinning chain animation to the throws

--- code/player.gd
+++ code/player.gd
@@ -12,6 +12,20 @@ var hp = max_hp
 @export var jump = "jump"
 @export var rotate_left = "rotate_left"
 @export var rotate_right = "rotate_right"
+var canPickUp = true
+
+var last_held_object: RigidBody3D
+var last_collided_object: RigidBody3D
+var is_colliding = false
+
+@onready var animation = $AnimationPlayer
+@onready var tight = $"MeshInstance3D/Chain3(straight-tight)"
+@onready var loose = $"MeshInstance3D/Chain4(snake-shaped)"
+@onready var ray = $RayCast3D
+
+@export var pick_up = "pick_up"
+@export var throw = "throw"
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -22,13 +36,14 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var look_dir: Vector2
 var camera_sens = 50
+var done_anim = true
+
 
 var is_sprinting = false
 
 func _ready():
-	#if !is_player_one:
-		#set_collision_layer_value(1, false)
-		#set_collision_layer_value(2, true)
+	ray.target_position = Vector3(0,0,10)
+	
 	pass
 
 func _physics_process(delta):
@@ -91,3 +106,55 @@ func _process(delta):
 			Score.score_1 += 1
 		if Score.score_1 < 3 && Score.score_2 < 3:
 			get_tree().reload_current_scene()
+	if Input.is_action_pressed(pick_up) and canPickUp and is_colliding:
+		loose.set_visible(false)
+		tight.set_visible(true)
+		last_collided_object.reparent($MeshInstance3D)
+		last_collided_object.freeze = true
+		last_collided_object.position = Vector3(0, 0, -2)
+		last_collided_object.rotation = Vector3(-90, 0, 0)
+		last_collided_object.set_collision_layer_value(7, false)
+		last_held_object = last_collided_object
+		canPickUp = false
+		is_colliding = false
+	
+	if Input.is_action_pressed(throw) && !canPickUp:
+		loose.set_visible(true)
+		tight.set_visible(false)
+		
+		
+		var distance = 10
+		var local_direction = Vector3(0, 0, -1)  # Local direction, forward
+		var world_direction = global_transform.basis * local_direction 
+		ray.target_position = world_direction * distance
+		var direction_vector = ray.get_target_position()
+		#direction_vector = direction_vector.normalized()  # Normalize for unit length
+		print(direction_vector)
+		
+		animation.play("spin_attack",-1,1,false)
+		
+		await get_tree().create_timer(.2666).timeout
+		
+		last_held_object.reparent(get_tree().root)
+		last_held_object.freeze = false
+		last_held_object.get_child(0).set_collision_mask_value(1, true)
+		last_held_object.get_child(0).set_collision_mask_value(2, true)
+		last_held_object.set_collision_layer_value(7, true)
+		last_held_object.set_axis_velocity(Vector3(direction_vector.x * 3, 0, direction_vector.z * 3))
+		canPickUp = true
+		
+		animation.play("recover",-1,1,false)
+		
+		await get_tree().create_timer(.07).timeout
+
+func _on_area_3d_body_entered(body):
+	is_colliding = true
+	last_collided_object = body
+
+
+func _on_area_3d_body_exited(body):
+	is_colliding = false
+
+
+
+	
